43591f7504e3d238a9f9469482a71841a61a24dd
Blues Henderson <bluesh322@gmail.com>
2024-09-11 10:12:18 -0600

    Mono repo for cSharpMasterClass, gonna put a lot of notes in the readme really

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello!");
+Console.WriteLine("Input the first number: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Input the second number: ");
+int number2 =  int.Parse(Console.ReadLine());
+Console.WriteLine("What do you want to do? ");
+Console.WriteLine("[A]dd numbers");
+Console.WriteLine("[S]ubtract numbers");
+Console.WriteLine("[M]ultiply numbers");
+string input = Console.ReadLine();
+
+if (input == "A" || input == "a")
+{
+    int result = number1 + number2;
+    Console.WriteLine($"{number1} + {number2} = {result}");
+}
+else if (input == "S" || input == "s")
+{
+    int result = number1 - number2;
+    Console.WriteLine($"{number1} - {number2} = {result}");
+}
+else if (input == "M" || input == "m")
+{
+    int result = number1 * number2;
+    Console.WriteLine($"{number1} * {number2} = {result}");
+}
+else
+{
+    Console.WriteLine("Invalid selection, sorry!");
+}
